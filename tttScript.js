5fe38ade112870799451d2502a4cd744b617c473
Helheim <helheimodin@nomail.com>
2021-05-03 01:05:54 +0800

    fully working, aesthetics not completed.

--- tttScript.js
+++ tttScript.js
@@ -8,7 +8,7 @@ const pbtn2 = document.querySelector("#btn2");
 let gridItems = document.querySelectorAll('.ticGridItem');
 let player1 = {name:"", choice:"", turn:false};
 let player2 = {name:"", choice:"", turn:false};
-let gameGrid = ['0','1','2','3','4','5','6','7','8'];
+let gameGrid = [];
 //init
 document.querySelector('#nPlayer1').value="";
 document.querySelector('#nPlayer2').value="";
@@ -31,7 +31,7 @@ btn_submit.addEventListener(
 
 pbtn1.addEventListener(
     'click', function(){
-        if(pbtn1.value=="X"||pbtn1.value=="x"){
+        if(pbtn1.value==="X"||pbtn1.value==="x"){
             pbtn1.value="O";
             pbtn2.value="X";
         }
@@ -44,7 +44,7 @@ pbtn1.addEventListener(
 
 pbtn2.addEventListener(
     'click', function(){
-        if (pbtn2.value=="X"||pbtn2.value=="x"){
+        if (pbtn2.value==="X"||pbtn2.value==="x"){
             pbtn2.value="O";
             pbtn1.value="X";
         }
@@ -69,10 +69,10 @@ function getPlayersData(){
     //puting the names
     player1.name = document.querySelector('#nPlayer1').value;
     player2.name = document.querySelector('#nPlayer2').value;
-    if(player1.name==""){
+    if(player1.name===""){
         player1.name = "Player 1";
     }
-    if(player2.name==""){
+    if(player2.name===""){
         player2.name = "Player 2";
     }
     document.querySelector('.player1 h1').innerHTML=player1.name;
@@ -94,14 +94,14 @@ function getPlayersData(){
 function gamePlacer(grid){
     if(document.querySelector('.'+grid.toString()).innerHTML==""){
         let adrs = grid.slice(-1);
-        if(player1.turn==true){
+        if(player1.turn===true){
             document.querySelector('.'+grid.toString()).innerHTML=player1.choice;
             gameGrid[adrs]=player1.choice;
             player1.turn=false;
             player2.turn=true;
             document.querySelector('.gameStatus h1').innerHTML = player2.name+"'s Turn!";
         }
-        else if(player2.turn==true){
+        else if(player2.turn===true){
             document.querySelector('.'+grid.toString()).innerHTML=player2.choice;
             gameGrid[adrs]=player2.choice;
             player1.turn=true;
@@ -130,7 +130,7 @@ function gameState(){
        for(adz=0; adz<3; adz++){
             same3 += gameGrid[winStates[wsi][adz]];
        }
-        if(same3 == player1.choice.repeat(3)){
+        if(same3 === player1.choice.repeat(3)){
             document.querySelector('.gameStatus h1').innerHTML = player1.name+" WINS!";
             let gridChilds = document.querySelector('.ticGameContainer').children;
             for(let i=0; i<gridChilds.length; i++){
@@ -138,7 +138,7 @@ function gameState(){
             }
             break;
         }
-        else if(same3 == player2.choice.repeat(3)){
+        else if(same3 === player2.choice.repeat(3)){
             document.querySelector('.gameStatus h1').innerHTML = player2.name+" WINS!";
             let gridChilds = document.querySelector('.ticGameContainer').children;
             for(let i=0; i<gridChilds.length; i++){
@@ -146,5 +146,18 @@ function gameState(){
             }
             break;
        }
+       else{
+            //draw
+            let counter=0;
+            for(i=0; i<gameGrid.length; i++){
+                if(gameGrid[i]==="X" || gameGrid[i]==="O"){
+                    counter++;
+                }
+            }
+            if(counter===9){
+                document.querySelector('.gameStatus h1').innerHTML ="GAME OVER!";
+            }
+        }
    }
+   
 }
